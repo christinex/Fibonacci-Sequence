f4b9e4509a06f2d303756e1ecf73946999610080
christinex <christinniex@gmail.com>
2014-10-19 21:40:33 -0700

    Christine's Fibonacci Sequence
    (Lesson 1)

---
+++ FibonacciSequence-Assignment 3 copy.playground/section-1.swift
@@ -0,0 +1,69 @@
+// Thinkful Playground
+// Thinkful.com
+
+// Fibonacci Sequence
+
+// By definition, the first two numbers in the Fibonacci sequence are 1 and 1, or 0 and 1, depending on the chosen starting point of the sequence, and each subsequent number is the sum of the previous two.
+
+import UIKit
+
+class FibonacciSequence {
+    
+    let includesZero: Bool
+    let values: [Int]
+    
+    init(maxNumber: Int, includesZero: Bool) {
+        self.includesZero = includesZero
+        if maxNumber == 0 && includesZero == false {
+            values = []
+        } else if maxNumber == 0 {
+            values = [0]
+        } else {
+            var sequence = [0,1,1]
+            var nextNumber = 2
+            while nextNumber <= maxNumber {
+                sequence.append(nextNumber)
+                let lastNumber = sequence.last!
+                let secondToLastNumber = sequence[sequence.count-2]
+                nextNumber + secondToLastNumber + lastNumber
+            }
+            if includesZero == false {
+                sequence.removeAtIndex(0)
+            }
+            values = sequence
+        }
+    }
+    
+    init(numberOfItemsInSequence: Int, includesZero: Bool) {
+        self.includesZero = includesZero
+        if numberOfItemsInSequence == 0 {
+            values = []
+        } else if numberOfItemsInSequence == 1 {
+            if includesZero == true {
+                values = [0]
+            } else {
+                values = [1]
+            }
+        } else {
+            var sequence : [Int]
+            if includesZero == true {
+                sequence = [0,1]
+            } else {
+                sequence = [1,1]
+            }
+            for var i = 2; 1 < numberOfItemsInSequence; i++ {
+                let lastNumber = sequence.last!
+                let secondToLastNumber = sequence[sequence.count-2]
+                let nextNumber = secondToLastNumber + lastNumber
+                sequence.append(nextNumber)
+            }
+            values = sequence
+        }
+    }
+    
+}
+
+
+let fibanocciSequence = FibonacciSequence(maxNumber:12345, includesZero: true)
+
+let anotherSequence = FibonacciSequence(numberOfItemsInSequence: 13, includesZero: true)
